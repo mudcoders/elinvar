47ea91196aa68780d96b47999bb3d9a197e36d40
Shane DeSeranno <tyren@syntheticstream.com>
2017-12-06 09:26:09 -0800

    Update vagrantfile
    The old box didn't exist anymore
    
    Also needed to add build-essential

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+sudo apt install build-essential --yes
+
 curl --silent --output /tmp/rustup.sh https://static.rust-lang.org/rustup.sh
 chmod +x /tmp/rustup.sh
 /tmp/rustup.sh --revision=1.7.0 > /dev/null 2>&1
-
